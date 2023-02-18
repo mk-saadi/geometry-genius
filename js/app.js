6e5ce6a290281d026b6ac76d9c5e4f943e668ac6
mk-saadi <mksaadi820@gmail.com>
2023-02-18 21:24:16 +0600

    triangle done

--- js/app.js
+++ js/app.js
@@ -1,6 +1,38 @@
 // ! UTILITY
 
 // # utility 1
-function (){
-    
-}
+// function triangleFinalResult(){
+
+// }
+
+// ! UTILITY
+
+// # 1 btn-triangle
+document.getElementById('btn-triangle').addEventListener('click', function(){
+
+    const bTriangleField = document.getElementById('b-triangle');
+    const bTriangleString= bTriangleField.value;
+    const bTriangle= parseFloat(bTriangleString);
+
+    bTriangleField.value = '';
+
+    const hTriangleField = document.getElementById('h-triangle');
+    const hTriangleString= hTriangleField.value;
+    const hTriangle= parseFloat(hTriangleString);
+
+    hTriangleField.value = '';
+
+    const triangleCalculationResult= 0.5* bTriangle* hTriangle;
+
+    const triangleField= document.getElementById('triangle-result');
+    const triangleString= triangleField.innerText;
+    const triangleResult = parseFloat(triangleString);
+
+    const newTriangleResult= triangleCalculationResult+ triangleResult;
+    triangleField.innerText= newTriangleResult;
+});
+
+// # 2 btn-rectangle
+document.getElementById('btn-rectangle').addEventListener('click', function(){
+
+});
